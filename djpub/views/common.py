1d28ef656664cc32cd08602a791ad9bd4844c808
Md Jahidul Hamid <jahidulhamid@yahoo.com>
2019-05-30 13:58:19 +0600

    update

--- djpub/views/common.py
+++ djpub/views/common.py
@@ -12,6 +12,7 @@ License: [BSD](http://www.opensource.org/licenses/bsd-license.php)
 
 from django.http import HttpResponse
 from django.views import View
+from django.utils.module_loading import import_string
 
 class BaseView(View):
     """Base view to be inherited by other views
